5074e2a4686794350d9966725f8862f3aa8674f7
rcuello <raul.cuello@sansano.usm.cl>
2024-06-26 20:12:45 -0400

    Estado de salud y de nutrientes OK

--- app-service/frontend-service/client/src/main.jsx
+++ app-service/frontend-service/client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Nutricion } from './components/UI/Nutricion';
+import { Nutricion } from './components/Nutricion';
+import { App } from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Nutricion />
+  <App />
 );
