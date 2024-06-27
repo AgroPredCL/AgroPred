2574e6b281ef4d087cdd164e5215a00d7b98709b
rcuello <raul.cuello@sansano.usm.cl>
2024-06-26 20:17:00 -0400

    actualización menor

--- app-service/frontend-service/client/src/App.jsx
+++ app-service/frontend-service/client/src/App.jsx
@@ -11,22 +11,21 @@ const data = [
       Descripcion: "Aparece en condiciones húmedas y cálidas.",
       Confiabilidad: 55,
     },
+  
     {
       fecha: "Febrero",
       Enfermedad: "Asfixia Raidicular",
       Impacto: "Alto",
       Descripcion: "Aparece en condiciones de alta humedad.",
       Confiabilidad: 75,
-  
     },
+
     {
       fecha: "Marzo",
       Enfermedad: "Deficiente Potasio",
       Impacto: "Medio",
       Descripcion: "Agregar fertilizante con potasio.",
       Confiabilidad: 85,
-  
-      
     },
   ];
   
@@ -40,35 +39,35 @@ const data = [
 export function App() {
     return (
         <>
-        <Seccion titulo='Estado de Salud'>
-            <div className='grid grid-cols-2 gap-4'>
-                <Table titulo='Actualidad' data={[]} fechas={[]} />
-                <Table titulo='Predicciones' filtro={true} data={data} fechas={fechas} confiabilidad='12' />
-            </div>
-        </Seccion>
+          <Seccion titulo='Estado de Salud'>
+              <div className='grid grid-cols-2 gap-4'>
+                  <Table titulo='Actualidad' data={[]} fechas={[]} />
+                  <Table titulo='Predicciones' filtro={true} data={data} fechas={fechas} confiabilidad='12' />
+              </div>
+          </Seccion>
 
-        <Seccion titulo='Estado Nutricional' actualizacion='Última lectura' ultimaLectura='24 junio 2024, 06:00'>
-            <div className="flex space-x-2 py-2 text-sm">
-                <Estado estado='Deficiente' valor='Deficiente' />
-                <Estado estado='Bajo' valor='Bajo' />
-                <Estado estado='Adecuado' valor='Adecuado' />
-                <Estado estado='Alto' valor='Alto' />
-                <Estado estado='Excesivo' valor='Excesivo' />
-            </div>
-            <div id="nutrientes" className="gap-8 py-2">
-                <Caracteristica titulo='Nitrógeno [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
-                <Caracteristica titulo='Fósforo [kg/ha]' estadoActual='Deficiente' valorActual='0.2' estadoPromedio='Adecuado' valorPromedio='0.8' />
-                <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
-                <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
-                <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
-                <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
-                <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
-            </div>
-        </Seccion>
+          <Seccion titulo='Estado Nutricional' actualizacion='Última lectura' ultimaLectura='24 junio 2024, 06:00'>
+              <div className="flex space-x-2 py-2 text-sm">
+                  <Estado estado='Deficiente' valor='Deficiente' />
+                  <Estado estado='Bajo' valor='Bajo' />
+                  <Estado estado='Adecuado' valor='Adecuado' />
+                  <Estado estado='Alto' valor='Alto' />
+                  <Estado estado='Excesivo' valor='Excesivo' />
+              </div>
+              <div id="nutrientes" className="gap-8 py-2">
+                  <Caracteristica titulo='Nitrógeno [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
+                  <Caracteristica titulo='Fósforo [kg/ha]' estadoActual='Deficiente' valorActual='0.2' estadoPromedio='Adecuado' valorPromedio='0.8' />
+                  <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
+                  <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
+                  <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
+                  <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
+                  <Caracteristica titulo='Potasio [kg/ha]' estadoActual='Adecuado' valorActual='0.8' estadoPromedio='Adecuado' valorPromedio='0.8' />
+              </div>
+          </Seccion>
 
-        <Seccion titulo='Predicción de Estado Nutricional'>
+          <Seccion titulo='Predicción de Estado Nutricional'>
 
-        </Seccion>
+          </Seccion>
         </>
     );
 }
