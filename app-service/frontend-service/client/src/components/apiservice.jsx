c04f8e102b4e72437b84d7a209519d906f66f282
Fran <francisca.henriquezr@sansano.usm.cl>
2024-06-27 00:05:50 -0400

    modificacion a main.py para acceder por react

--- app-service/frontend-service/client/src/components/apiservice.jsx
+++ app-service/frontend-service/client/src/components/apiservice.jsx
@@ -10,7 +10,6 @@ export const fetchData = async (ruta) => {
   try {
     console.log("Mmmmmm raro")
     const response = await api.get(ruta); // Reemplaza '/ruta' con la ruta específica de tu API
-    console.log(response)
     return response.data;
   } catch (error) {
     console.error('Error fetching data', error);
