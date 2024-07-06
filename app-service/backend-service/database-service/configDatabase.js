46ec1914e8d96b48436d27c58a8d57307870f921
hannah <felipe.sanmartini@sansano.usm.cl>
2024-07-05 20:10:42 -0400

    se eliminaron archivos y directorios desactualizados (relacionados a crear la db y la API sin sequelize)

--- app-service/backend-service/database-service/configDatabase.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    user: 'postgres',
-    host: 'localhost',
-    password: '1234',
-    port: 5432, // Default PostgreSQL port
-    
-};
--- app-service/backend-service/database-service/configTables.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'agropred',
-    password: '1234',
-    port: 5432, // Default PostgreSQL port
-};
