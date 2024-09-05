ccd3f578906e8c7d94373ce146f86ee0d18a8fb1
hannah <felipe.sanmartini@sansano.usm.cl>
2024-09-05 08:38:45 -0400

    fix?

--- frontend-service/client/src/pages/EstadoCuarteles.jsx
+++ frontend-service/client/src/pages/EstadoCuarteles.jsx
@@ -77,7 +77,7 @@ export default function EstadoCuarteles() {
                 <article className="mt-6 pt-2 border-t-2">
                     <h2 className="text-xl font-semibold text-gray-700 mb-4">Recomendación de Fertilizantes</h2>
                     <div className="flex justify-between flex-wrap">
-                        {Object.entries(dataRecomendaciones).map(([elemento, recomendacion], index) => (
+                        {dataRecomendaciones && Object.entries(dataRecomendaciones).map(([elemento, recomendacion], index) => (
                         <div key={index} className="w-full md:w-1/3 lg:w-1/4 p-2">
                             <div className={`p-4 shadow-lg rounded-lg hover:shadow-xl transition-shadow h-full ${colores[elemento] || 'bg-gray-100'}`}>
                                 <h3 className="text-lg font-semibold text-gray-800 capitalize">{elemento}</h3>
