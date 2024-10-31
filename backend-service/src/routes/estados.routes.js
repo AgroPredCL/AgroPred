1c9796bb4330f69de03e6bc77f3b609d94d76745
hannah <felipe.sanmartini@sansano.usm.cl>
2024-10-31 11:31:24 -0300

    fixes usos_riegos y estados

--- backend-service/src/routes/estados.routes.js
+++ backend-service/src/routes/estados.routes.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import { Router } from "express";
-import { getEstados,createEstado,deleteEstado,updateEstado,getEstadoByFecha } from "../controllers/estados.controller.js";
-
-
-const router = Router()
-
-router.get('/estados',getEstados);
-
-router.post('/estado',createEstado);
-
-router.delete('/estado/:fecha',deleteEstado);
-
-router.put('/estado/:fecha',updateEstado);
-
-router.get('/estado/:fecha',getEstadoByFecha);
-
-
-=======
 import { Router } from "express";
 import { getEstados,createEstado,deleteEstado,updateEstado, getEstadoById } from "../controllers/estados.controller.js";
 
@@ -34,5 +15,4 @@ router.put('/estado/:id/:cuartel_ID',updateEstado);
 router.get('/estado/:id/:cuartel_ID',getEstadoById);
 
 
->>>>>>> origin/feature_fechas_tildes
 export default router;
--- backend-service/src/routes/usos_riegos.routes.js
+++ backend-service/src/routes/usos_riegos.routes.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { Router } from "express";
-
-import { getUsos_Riegos,createUso_Riego,deleteUso_Riego,updateUso_Riego,getUso_RiegoByCuartel } from "../controllers/usos_riegos.controller.js";
-
-
-const router = Router();
-
-router.get('/usos_riegos',getUsos_Riegos);
-
-router.post('/uso_riego',createUso_Riego);
-
-router.delete('/uso_riego/:fecha',deleteUso_Riego);
-
-router.put('/uso_riego/:fecha',updateUso_Riego);
-
-router.get('/uso_riego/:cuartel_ID', getUso_RiegoByCuartel);
-
-
-=======
 import { Router } from "express";
 
 import { getUsos_Riegos,createUso_Riego,deleteUso_Riego,updateUso_Riego,getUso_RiegoByCuartel } from "../controllers/usos_riegos.controller.js";
@@ -36,5 +16,4 @@ router.put('/uso_riego/:id',updateUso_Riego);
 router.get('/uso_riego/:cuartel_ID/:fechaInicio/:fechaFin', getUso_RiegoByCuartel);
 
 
->>>>>>> origin/feature_fechas_tildes
 export default router;
