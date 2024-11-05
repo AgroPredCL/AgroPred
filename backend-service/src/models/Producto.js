f3fd955e3ac8faf145d9c47040221c9c98aab605
hannah <felipe.sanmartini@sansano.usm.cl>
2024-11-05 16:26:09 -0300

    fix Producto.js merge conflict (?)

--- backend-service/src/models/Producto.js
+++ backend-service/src/models/Producto.js
@@ -1,41 +1,25 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../database/database.js";
-import { Tag } from "./Tags.js";
-import { ProductoTag } from "./Producto_Tags.js";
+import { DataTypes } from "sequelize"
+import  sequelize  from "../database/database.js"
 
-export const Producto = sequelize.define('producto', {
-    producto_ID: {
+export const Producto = sequelize.define('producto',{
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    categoria: {
+    categoria : {
         type: DataTypes.STRING(30),
         allowNull: false
     },
     cantidad : {
-    vencimiento: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-    },
-    cantidad: {
         type: DataTypes.INTEGER,
         allowNull: false
     },
     descripcion : {
-    costo: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    descripcion: {
         type: DataTypes.STRING(100)
     },
     estado : {
         type: DataTypes.STRING(100)
-    },
-    nombre: {
-        type: DataTypes.STRING(25),
-        allowNull: false
     },
     nombre:{
         type: DataTypes.STRING(25),
@@ -44,15 +28,11 @@ export const Producto = sequelize.define('producto', {
     ubicacion: {
         type: DataTypes.STRING(40)
     }
-});
+   
+    
+  
 
-// Definición de relaciones
-Producto.hasMany(ProductoTag, {
-    foreignKey: 'producto_ID',
-    sourceKey: 'producto_ID'   
-});
-
-ProductoTag.belongsTo(Producto, {
-    foreignKey: 'producto_ID', 
-    targetKey: 'producto_ID'   
-});
+ 
+    
+    
+}) 
