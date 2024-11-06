2b08585071ff8446ce6e3db2f2f3f391ffbe24b0
JoacoFierro <joaquin.fierro@sansano.usm.cl>
2024-11-05 22:23:28 -0300

    Controlador -Usuario - Joaco

--- backend-service/src/controllers/usuarios.controller.js
+++ backend-service/src/controllers/usuarios.controller.js
@@ -16,14 +16,15 @@ export const getUsuarios = async (req, res) => {
 // Crear un nuevo usuario
 export const createUsuario = async (req, res) => {
     try {
-        const { rut, email, nom_predio, full_name, num_telefono, password } = req.body;
+        const { rut, email, nom_predio, full_name, num_telefono, password,estado } = req.body;
         const newUsuario = await Usuario.create({
             rut, // Agregar rut como parte de los datos del usuario
             email,
             nom_predio,
             full_name,
             num_telefono,
-            password
+            password,
+            estado
         });
         res.json(newUsuario);
     } catch (error) {
