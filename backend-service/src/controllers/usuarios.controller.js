853e3f0303d3ba4619799b9905af987d70659eb0
hannah <felipe.sanmartini@sansano.usm.cl>
2024-11-05 23:06:58 -0300

    pequeño fix a usuarios.controller.js (faltaba rol)

--- backend-service/src/controllers/usuarios.controller.js
+++ backend-service/src/controllers/usuarios.controller.js
@@ -16,7 +16,7 @@ export const getUsuarios = async (req, res) => {
 // Crear un nuevo usuario
 export const createUsuario = async (req, res) => {
     try {
-        const { rut, email, nom_predio, full_name, num_telefono, password,estado } = req.body;
+        const { rut, email, nom_predio, full_name, num_telefono, password, estado, rol } = req.body;
         const newUsuario = await Usuario.create({
             rut, // Agregar rut como parte de los datos del usuario
             email,
@@ -24,7 +24,8 @@ export const createUsuario = async (req, res) => {
             full_name,
             num_telefono,
             password,
-            estado
+            estado,
+            rol
         });
         res.json(newUsuario);
     } catch (error) {
