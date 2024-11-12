326bfcf12e27df949adfd0b4d86e0817a17158b9
rcuello <raul.cuello@sansano.usm.cl>
2024-11-12 17:19:13 -0300

    imagen en inicio

--- frontend-service/client/src/pages/Home.jsx
+++ frontend-service/client/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
 	return (
 		<div className='p-4'>
-			<p>somos agropred!</p>
+			<p>Imagen referencial</p>
+			<img src='/predio.jpg' alt='agrícola' />
 		</div>
 	);
 }
